ae76b87a656966860ff7378ea99f011db638eb41
shivam7042263411-coder <shivam7042263411@gmail.com>
2026-05-01 15:57:13 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+configure<CloudstreamExtension> {
+    // This creates the 'builds' folder we need
+    setDestConfig("builds")
+}
