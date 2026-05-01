ba391ee560ad893c98ad340b8a87970843ce4c38
shivam7042263411-coder <shivam7042263411@gmail.com>
2026-05-01 16:06:12 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,9 @@ apply(plugin = "com.android.library")
 apply(plugin = "kotlin-android")
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-// Simplified configuration
+// We use the simplest possible call here
 configure<com.lagradost.cloudstream3.gradle.CloudstreamExtension> {
-    // We removed 'destination =' and replaced it with this:
-    setDestConfig("builds")
+    // No destination or setDestConfig lines - let it use defaults
 }
 
 configure<com.android.build.gradle.LibraryExtension> {
